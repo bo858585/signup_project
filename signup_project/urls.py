a884e8f5012494f7766053dc707f01c553808ce3
bo858585 <bo858585@ubuntu.(none)>
2014-01-03 23:55:50 +0200

    base.html fix

--- signup_project/urls.py
+++ signup_project/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
-from sms_signup.views import LoginView
+from sms_signup.views import LoginView, PasswordRecoveryView
 
 
 # Uncomment the next two lines to enable the admin:
@@ -14,6 +14,8 @@ urlpatterns = patterns('',
     url(r'^signup/', include('sms_signup.urls')),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
     url(r'^login/$',  LoginView.as_view(), name='login'),
+    url(r'^forgot_password/$',  PasswordRecoveryView.as_view(), name='forgot_password'),
+    url(r'^password_sent/$',  TemplateView.as_view(template_name = "password_sent.html"), name='password_sent'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
